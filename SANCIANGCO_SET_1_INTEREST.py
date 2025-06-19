82ce9d48d0429e598e959c71a654264a88054006
Nathan Sanciangco <nathan.roevesen.sanciangco@student.ateneo.edu>
2025-06-19 10:39:14 +0800

    Set 1 code

---
+++ SANCIANGCO_SET_1_INTEREST.py
@@ -0,0 +1,34 @@
+'''Programming Set 1
+
+This assignment will familiarize you with Python's basics.
+'''
+
+def interest(principal, rate, periods):
+    '''Interest.
+
+    This function calculates the final value of an investment after
+        gaining simple interest over a number of periods.
+
+    To calculate simple interest, simply multiply the principal to the quantity (rate * time).
+        Add this amount to the principal to get the final value.
+
+    Round down the final amount.
+
+    Parameters
+    ----------
+    principal: int
+        the principal (i.e., starting) amount invested, expressed in centavos
+    rate: float
+        the interest rate per period, expressed as a decimal representation of a percentage (e.g., 3% is 0.03)
+    periods: int
+        the number of periods invested
+
+    Returns
+    -------
+    int
+        the final value of the investment
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    final_investment = principal + int(principal*(rate*periods))
+    return final_investment
